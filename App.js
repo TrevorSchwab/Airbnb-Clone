2232628a4cc47529d1feb789d3d6d74f6bc92001
Trevor Schwab <trevschwab@gmail.com>
2021-04-25 18:18:46 -0600

    Project initialized and file structure set up

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+}
